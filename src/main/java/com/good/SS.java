04fc5f3c95eacb3a54da81f61e4ff4d8bd0f7c68
mmm <1059751326@qq.om>
2016-01-25 22:41:31 +0800

    1.25提交

---
+++ src/main/java/com/good/SS.java
@@ -0,0 +1,14 @@
+package com.good;
+
+/**
+ * Created by Administrator
+ * Date:2016/1/10
+ */
+public class SS {
+    private int a = 0;
+
+    public void b(){
+        a += 1;
+        System.out.println(a+"=========================");
+    }
+}
