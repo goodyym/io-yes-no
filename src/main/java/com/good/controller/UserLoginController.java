cbd096d11ff7ca40e2ce3a8375779f657c40d4aa
yym123 <ym.yuan@haitongwangxiao.com>
2016-01-26 17:42:17 +0800

    1.26更新

--- src/main/java/com/good/controller/UserLoginController.java
+++ src/main/java/com/good/controller/UserLoginController.java
@@ -29,11 +29,26 @@ public class UserLoginController {
     @RequestMapping("/welcome")
     public String welcome(HttpServletRequest request){
         HttpSession ss2 = request.getSession();
+        ss2.setAttribute("yy","mmmValue111");
+
+
         System.out.println(ss2.getId()+"======welcome====sessionId");
-        System.out.println(ss2.getAttribute("yy"));
+
+        System.out.println(ss2.getAttribute("yy")+"===welcome");
 
 
         return "welcome";
+    }
+
+    @RequestMapping("/welcome2")
+    public void welcome2(HttpServletRequest request){
+        HttpSession ss2 = request.getSession();
+
+        System.out.println(ss2.getId()+"======welcome2222222====sessionId");
+
+        System.out.println(ss2.getAttribute("yy")+"===welcome2222222");
+
+
     }
 
 }
